21bfcbdbf76b1d6c10016559dedbee54f3258796
Murillo Nicacio <mnmaraes@gmail.com>
2019-12-02 17:10:14 +0000

    Adds kea compat options

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 // @flow
 import NodeDB, { nodeDBReducer, createNodeDBReducer } from './store';
-import { withNode, withoutNode, withIndexedNodes } from './enhancers';
+import { withNode, withoutNode, withIndexedNodes, configureNodeDBEnhancer } from './enhancers';
 import { nodeSetToNodeMap } from './utils/nodes';
+
 export default NodeDB;
+
 export {
   createNodeDBReducer,
+  configureNodeDBEnhancer,
   nodeDBReducer,
   withNode,
   withIndexedNodes,
